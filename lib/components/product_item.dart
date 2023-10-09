e375581d2ede3b85501157eee1b53da6759f9b8d
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com>
2023-10-09 12:23:01 -0300

    :sparkles: Feat(New provider and Detail Page)

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:store/model/product.dart';
+import 'package:store/pages/product_detail_page.dart';
+import 'package:store/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
   final Product product;
@@ -26,9 +28,15 @@ class ProductItem extends StatelessWidget {
           ),
         ),
       ),
-      child: Image.network(
-        product.imageUrl,
-        fit: BoxFit.cover,
+      child: GestureDetector(
+        onTap: () => Navigator.of(context).pushNamed(
+          AppRoutes.PRODUCT_DETAIL,
+          arguments: product,
+        ),
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:store/pages/counter_page.dart';
+import 'package:store/pages/product_detail_page.dart';
 import 'package:store/pages/products_overview_page.dart';
+import 'package:store/providers/counter.dart';
+import 'package:store/utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,15 +14,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Store demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        fontFamily: 'Lato',
+    return CounterProvider(
+      child: MaterialApp(
+        title: 'Store demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          fontFamily: 'Lato',
+        ),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (ctx) => const CounterPage(),
+        },
+        debugShowCheckedModeBanner: false,
+        home: ProductsOverview(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: ProductsOverview(),
     );
   }
 }
