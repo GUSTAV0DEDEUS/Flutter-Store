7f2573a019b06080b85746738c3eb5742b12c891
GUSTAV0DEDEUS <gustavodedeusconceicao@gmail.com>
2023-10-10 01:11:19 -0300

    :recycle: Refactor(Changed to Provider)

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:store/model/product.dart';
-import 'package:store/pages/product_detail_page.dart';
 import 'package:store/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
-  final Product product;
-  const ProductItem({Key? key, required this.product}) : super(key: key);
+  const ProductItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: false);
     return GridTile(
       footer: GridTileBar(
         backgroundColor: Colors.black87,
         title: Text(product.title),
-        leading: IconButton(
-          onPressed: null,
-          icon: Icon(
-            Icons.favorite,
-            color: Theme.of(context).colorScheme.primary,
-          ),
-        ),
+        leading: Consumer<Product>(builder: (ctx, product, _) {
+          return IconButton(
+            onPressed: () => product.toggleFavorite(),
+            icon: Icon(
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          );
+        }),
         trailing: IconButton(
           onPressed: null,
           icon: Icon(
